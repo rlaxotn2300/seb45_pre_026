e4a2ff587b939aa87f0c9bf07ba1ae9e2bc804bb
Jess-Apr <jessie1944@naver.com>
2023-08-10 16:07:35 +0900

    feat: 헤더 완성

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import '../css/header.css';
+import '../css/component.css';
 import Logo from '../images/Logo.png';
 import QuestionsLogo from '../images/questions-logo.png';
+import Mypage from '../images/mypage.png';
 
 export default function Header() {
   const [loggedin, setIsLoggedin] = useState(false);
@@ -14,29 +16,36 @@ export default function Header() {
     <div className="header__container">
       <div className="header__left">
         <img className="header__logo" src={Logo} alt="로고" />
-        <div>
+        <div className="header__name">
           <span>Code</span>
-          <span>Knitters</span>
+          <span className="header__name-strong">Knitters</span>
         </div>
-        <div>
+        <div className="header__questions">
           <img
             className="header__questions-logo"
             src={QuestionsLogo}
             alt="질문 로고"
           />
-          <span>Questions</span>
+          <span className="header__questions-name">Questions</span>
         </div>
       </div>
-      <div>
+      <div className="header__right">
         {loggedin ? (
-          <img alt="마이페이지" />
+          <img className="header__mypage" src={Mypage} alt="마이페이지" />
         ) : (
-          <button onClick={handleLoginClick}></button>
+          <button
+            className="button-light header__login-btn"
+            onClick={handleLoginClick}
+          >
+            Log in
+          </button>
         )}
         {loggedin ? (
-          <button onClick={handleLoginClick}></button>
+          <button className="button-dark" onClick={handleLoginClick}>
+            Log out
+          </button>
         ) : (
-          <button></button>
+          <button className="button-dark">Sign up</button>
         )}
       </div>
     </div>
