a062c6774d37cabfa74a214c0826034bc6a715a3
Jess-Apr <jessie1944@naver.com>
2023-08-10 11:09:57 +0900

    fix: 충돌된 문서 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-          <Routes>
-            <Route />
-          </Routes>
+        <Routes>
+          <Route />
+        </Routes>
       </BrowserRouter>
     </div>
   );
