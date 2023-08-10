0ed71b9477d594306c8bdf73a0ad6a3c8c21110c
jina <oksu010@mail.com>
2023-08-11 02:12:20 +0900

    fix: 포스트맨에러수정[be02]

--- server/src/main/java/com/preproject/stackOverflow/question/entity/Question.java
+++ server/src/main/java/com/preproject/stackOverflow/question/entity/Question.java
@@ -30,19 +30,19 @@ public class Question {
     private String content;
 
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    @Column
-    private Member member;
-
-    public void setMember(Member member){
-        this.member = member;
-    }
-
-
-
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
-    private List<Answer> answerList = new ArrayList<>();
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    @Column
+//    private Member member;
+//
+//    public void setMember(Member member){
+//        this.member = member;
+//    }
+//
+//
+//
+//    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+//    private List<Answer> answerList = new ArrayList<>();
 
 
 
@@ -58,7 +58,7 @@ public class Question {
 
     @ElementCollection
     @CollectionTable(name = "QUESTION_TAG_LIST", joinColumns = @JoinColumn(name = "QUESTION_ID"))
-    @Column(name = "TAG_BODY")
+    @Column(name = "TAG_LIST")
     private List<String> tagList;
 
 
