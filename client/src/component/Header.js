ff946cee3f6f11aa8f7d32c09a51113f17a4e30d
rlaxotn2300 <rlaxotn2300@naver.com>
2023-08-11 17:07:55 +0900

    feat:헤더 스타일 수정 [FE01]

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -23,59 +23,61 @@ function Header({ setCurPage }) {
 
   return (
     <div className="header__container">
-      <div className="header__left">
-        <Link to="/" className="link header__left">
-          <img className="header__logo" src={Logo} alt="로고" />
-          <div className="header__name">
-            <span>Code</span>
-            <span className="header__name-strong">Knitters</span>
-          </div>
-        </Link>
-        <Link to="/questions" className="link">
-          <div
-            className="header__questions"
-            role="presentation"
-            onClick={() => setCurPage('questions')}
-          >
-            <img
-              className="header__questions-logo"
-              src={QuestionsLogo}
-              alt="질문 로고"
-            />
-            <span className="header__questions-name">Questions</span>
-          </div>
-        </Link>
-      </div>
-      <div className="header__right">
-        {loggedin ? (
-          <Link to="/mypage" className="link">
-            <img
-              className="header__mypage"
-              src={Mypage}
-              alt="마이페이지"
+      <div className="header_wrap">
+        <div className="header__left">
+          <Link to="/" className="link header__left">
+            <img className="header__logo" src={Logo} alt="로고" />
+            <div className="header__name">
+              <span>Code</span>
+              <span className="header__name-strong">Knitters</span>
+            </div>
+          </Link>
+          <Link to="/questions" className="link">
+            <div
+              className="header__questions"
               role="presentation"
-              onClick={() => setCurPage('user')}
-            />
-          </Link>
-        ) : (
-          <Link to="/login" className="link">
-            <button
-              className="button-light header__login-btn"
-              onClick={handleLoginClick}
+              onClick={() => setCurPage('questions')}
             >
-              Log in
+              <img
+                className="header__questions-logo"
+                src={QuestionsLogo}
+                alt="질문 로고"
+              />
+              <span className="header__questions-name">Questions</span>
+            </div>
+          </Link>
+        </div>
+        <div className="header__right">
+          {loggedin ? (
+            <Link to="/mypage" className="link">
+              <img
+                className="header__mypage"
+                src={Mypage}
+                alt="마이페이지"
+                role="presentation"
+                onClick={() => setCurPage('user')}
+              />
+            </Link>
+          ) : (
+            <Link to="/login" className="link">
+              <button
+                className="button-light header__login-btn"
+                onClick={handleLoginClick}
+              >
+                Log in
+              </button>
+            </Link>
+          )}
+          {loggedin ? (
+            <button className="button-dark" onClick={handleLoginClick}>
+              Log out
             </button>
-          </Link>
-        )}
-        {loggedin ? (
-          <button className="button-dark" onClick={handleLoginClick}>
-            Log out
-          </button>
-        ) : (
-          <Link to="/sign_up">
-            <button className="button-dark">Sign up</button>
-          </Link>
-        )}
+          ) : (
+            <Link to="/sign_up">
+              <button className="button-dark">Sign up</button>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
