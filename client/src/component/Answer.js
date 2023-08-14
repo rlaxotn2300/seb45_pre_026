4f2546840537f86777621aa0707ddafcf4239260
Jess-Apr <jessie1944@naver.com>
2023-08-14 20:17:26 +0900

    feat: 답변 기능 구현 완료[fe02]

--- client/src/component/Answer.js
+++ client/src/component/Answer.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import '../css/answer.css';
+import '../css/component.css';
 
 export default function Answer({ questionData }) {
   let { id } = useParams();
@@ -7,21 +8,32 @@ export default function Answer({ questionData }) {
   return (
     <div className="answer__bg">
       {questionData[id].answer.length > 1 ? (
-        <div>{questionData[id].answer.length} Answers</div>
+        <div className="answer__title">
+          {questionData[id].answer.length} Answers
+        </div>
       ) : questionData[id].answer.length === 1 ? (
-        <div>{questionData[id].answer.length} Answer</div>
+        <div className="answer__title">
+          {questionData[id].answer.length} Answer
+        </div>
       ) : null}
       {questionData[id].answer
         ? questionData[id].answer.map((el) => (
-            <li key={el.answerId}>
-              <div>{el.content}</div>
-              <div>
+            <div key={el.answerId} className="list answer__content-container">
+              <div className="answer__content">{el.content}</div>
+              <div className="answer__bottom">
                 <div>{el.date}</div>
                 <div>Answered by {el.user}</div>
               </div>
-            </li>
+            </div>
           ))
         : null}
+      <div className="answer__input-container">
+        <div className="answer__input-title">Your Answer</div>
+        <textarea className="input answer__input"></textarea>
+        <button className="button-dark answer__post-btn">
+          Post Your Answer
+        </button>
+      </div>
     </div>
   );
 }
