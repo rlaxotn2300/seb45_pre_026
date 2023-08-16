244d5585b23b2621a45437d818edcb7f13452c36
Jess-Apr <jessie1944@naver.com>
2023-08-16 17:47:32 +0900

    style: aside 컴포넌트 a 태그 스타일 수정[fe02]

--- client/src/component/Aside.js
+++ client/src/component/Aside.js
@@ -22,103 +22,129 @@ const Aside = () => {
           <a
             href="https://developer.mozilla.org/ko/docs/Web/HTML"
             target="blank"
+            className="link"
           >
             <img src={html} alt="html"></img>
-            <p>HTML</p>
+            <p className="aside__focus">HTML</p>
           </a>
         </li>
         <li>
           <a
             href="https://developer.mozilla.org/ko/docs/Web/CSS/Reference"
             target="blank"
+            className="link"
           >
             <img src={css} alt="css"></img>
 
-            <p>CSS</p>
+            <p className="aside__focus">CSS</p>
           </a>
         </li>
         <li>
-          <a href="https://ko.javascript.info/" target="blank">
+          <a href="https://ko.javascript.info/" target="blank" className="link">
             <img src={js} alt="js"></img>
-            <p>JavaScript</p>
+            <p className="aside__focus">JavaScript</p>
           </a>
         </li>
         <li>
           <a
             href="https://ko.legacy.reactjs.org/docs/getting-started.html"
             target="blank"
+            className="link"
           >
             <img src={react} alt="react"></img>
-            <p>React</p>
+            <p className="aside__focus">React</p>
           </a>
         </li>
         <li>
-          <a href="https://axios-http.com/kr/docs/intro" target="blank">
+          <a
+            href="https://axios-http.com/kr/docs/intro"
+            target="blank"
+            className="link"
+          >
             <img src={axios} alt="axios"></img>
-            <p>Axios</p>
+            <p className="aside__focus">Axios</p>
           </a>
         </li>
         <li>
-          <a href="https://ko.redux.js.org/" target="blank">
+          <a href="https://ko.redux.js.org/" target="blank" className="link">
             <img src={redux} alt="redux"></img>
-            <p>Redux</p>
+            <p className="aside__focus">Redux</p>
           </a>
         </li>
         <li>
-          <a href="https://eslint.org/docs/latest/" target="blank">
+          <a
+            href="https://eslint.org/docs/latest/"
+            target="blank"
+            className="link"
+          >
             <img src={eslint} alt="eslint"></img>
-            <p>ESLint</p>
+            <p className="aside__focus">ESLint</p>
           </a>
         </li>
         <li>
-          <a href="https://prettier.io/docs/en/" target="blank">
+          <a
+            href="https://prettier.io/docs/en/"
+            target="blank"
+            className="link"
+          >
             <img src={prettier} alt="prettier"></img>
-            <p>Prettier</p>
+            <p className="aside__focus">Prettier</p>
           </a>
         </li>
         <li>
-          <a href="https://docs.oracle.com/javase/8/docs/api/" target="blank">
+          <a
+            href="https://docs.oracle.com/javase/8/docs/api/"
+            target="blank"
+            className="link"
+          >
             <img src={java} alt="java"></img>
-            <p>JAVA</p>
+            <p className="aside__focus">JAVA</p>
           </a>
         </li>
         <li>
           <a
             href="https://docs.spring.io/spring-framework/reference/index.html"
             target="blank"
+            className="link"
           >
             <img src={spring} alt="spring"></img>
-            <p>Spring</p>
+            <p className="aside__focus">Spring</p>
           </a>
         </li>
         <li>
           <a
             href="https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/"
             target="blank"
+            className="link"
           >
             <img src={springBoot} alt="springBoot"></img>
-            <p>SpringBoot</p>
+            <p className="aside__focus">SpringBoot</p>
           </a>
         </li>
         <li>
           <a
             href="https://docs.spring.io/spring-security/reference/index.html"
             target="blank"
+            className="link"
           >
             <img src={springSecurity} alt="springSecurity"></img>
-            <p>SpringSecurity</p>
+            <p className="aside__focus">SpringSecurity</p>
           </a>
         </li>
         <li>
-          <a href="https://dev.mysql.com/doc/" target="blank">
+          <a href="https://dev.mysql.com/doc/" target="blank" className="link">
             <img src={mysql} alt="mysql"></img>
-            <p>MySQL</p>
+            <p className="aside__focus">MySQL</p>
           </a>
         </li>
         <li>
-          <a href="https://docs.aws.amazon.com/ko_kr/" target="blank">
+          <a
+            href="https://docs.aws.amazon.com/ko_kr/"
+            target="blank"
+            className="link"
+          >
             <img src={aws} alt="aws"></img>
-            <p>AWS</p>
+            <p className="aside__focus">AWS</p>
           </a>
         </li>
       </ul>
