5a5c8cb1c4b19f39af4f101b36d335fe786e92e2
Jess-Apr <jessie1944@naver.com>
2023-08-16 13:09:24 +0900

    feat: 개별 질문 페이지 구현중, Aside 컴포넌트 css 수정[fe02]

--- client/src/page/QuestionDetail.js
+++ client/src/page/QuestionDetail.js
@@ -1,14 +1,43 @@
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Answer from '../component/Answer';
+import Nav from '../component/Nav';
+import Aside from '../component/Aside';
 import { questionData } from '../dummydata';
 import '../css/questionDetail.css';
+import edit from '../images/edit.png';
 
 export default function QuestionDetail() {
-  //   let { id } = useParams();
+  let { id } = useParams();
 
   return (
     <div className="detail__bg">
-      <Answer questionData={questionData} />
+      <Nav />
+      <div className="detail__main">
+        <div className="detail__top">
+          <div className="detail__title-container">
+            <div className="detail__title">{questionData[id].title}</div>
+            <div className="detail__btn-container">
+              <button className="detail__btn">Delete</button>
+              <button className="detail__btn">
+                <img alt="질문 수정" src={edit} />
+                <span>Edit</span>
+              </button>
+            </div>
+          </div>
+          <div>Asked {questionData[id].date}</div>
+        </div>
+        <div className="detail__content-wrap">
+          <div className="detail__content-container">
+            <div>{questionData[id].content}</div>
+            <div>
+              <div></div>
+              <div>{questionData[id].user}</div>
+            </div>
+            <Answer questionData={questionData} />
+          </div>
+          <Aside />
+        </div>
+      </div>
     </div>
   );
 }
