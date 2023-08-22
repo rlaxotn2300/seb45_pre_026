614d65a1899f56eafe7640af5319cf0118ce0f52
jiwon <sahel526@gmail.com>
2023-08-22 16:23:12 +0900

    .

--- server/src/test/java/com/preproject/stackOverflow/StackOverflowApplicationTests.java
+++ server/src/test/java/com/preproject/stackOverflow/StackOverflowApplicationTests.java
@@ -1,13 +1,13 @@
-package com.preproject.stackOverflow;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StackOverflowApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.preproject.stackOverflow;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class StackOverflowApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
