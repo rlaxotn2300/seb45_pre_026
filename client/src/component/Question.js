4b43023c01dc897ffc6d91f3f6b4c753489d6309
rlaxotn2300 <rlaxotn2300@naver.com>
2023-08-22 15:19:20 +0900

    fix:question.js 스타일수정 fe01

--- client/src/component/Question.js
+++ client/src/component/Question.js
@@ -10,35 +10,33 @@ function Question({ data, answer }) {
   // console.log(answer_filter);
 
   return (
-    <div>
-      <div className="question__container" key={data.questionId}>
-        <div className="question__side">
-          <div>{data.vote} votes</div>
-          <div
-            className={
-              answer_filter?.length === 0
-                ? 'quesiton__no-answer'
-                : 'question__answer'
-            }
-          >
-            {answer_filter?.length} answers
-          </div>
+    <div className="question__container" key={data.questionId}>
+      <div className="question__side">
+        <div>{data.vote} votes</div>
+        <div
+          className={
+            answer_filter?.length === 0
+              ? 'quesiton__no-answer'
+              : 'question__answer'
+          }
+        >
+          {answer_filter?.length} answers
         </div>
-        <div className="question__main">
-          <div className="question__content-container">
-            <div className="question__title">{data.title}</div>
-            <div className="question__content">{contentWithNoSpace}</div>
+      </div>
+      <div className="question__main">
+        <div className="question__content-container">
+          <div className="question__title">{data.title}</div>
+          <div className="question__content">{contentWithNoSpace}</div>
+        </div>
+        <div className="question__bottom">
+          <div className="tag_box">
+            {data.tags.map((el) => (
+              <span key={el}>{el}</span>
+            ))}
           </div>
-          <div className="question__bottom">
-            <div className="tag_box">
-              {data.tags.map((el) => (
-                <span key={el}>{el}</span>
-              ))}
-            </div>
-            <div className="question__author">
-              <div className="question__author-photo"></div>
-              {/* <div>{data.user}</div> */}
-            </div>
+          <div className="question__author">
+            <div className="question__author-photo"></div>
+            {/* <div>{data.user}</div> */}
           </div>
         </div>
       </div>
