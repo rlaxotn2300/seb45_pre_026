443173fba00a24ecde875f94d1799398ac5f5d31
Jess-Apr <jessie1944@naver.com>
2023-08-23 09:34:40 +0900

    feat: 배포 전 마무리[fe02]

--- client/src/component/Answer.js
+++ client/src/component/Answer.js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { Cookies } from 'react-cookie';
 import '../css/answer.css';
 import '../css/component.css';
 import Vote from './Vote';
 
-export default function Answer({ questionData }) {
+export default function Answer() {
   const [answerList, setAnswerList] = useState([]);
   const [answer, setAnswer] = useState('');
   const [isAnswerEmpty, setIsAnswerEmpty] = useState(false);
+  const cookies = new Cookies();
+  const getCookie = cookies.get('is_login');
+  const memberId = window.localStorage.getItem('memberId');
   let { id } = useParams();
 
   function handleAnswerChange(e) {
@@ -20,15 +24,18 @@ export default function Answer({ questionData }) {
     setIsAnswerEmpty(false);
 
     if (answer === '') setIsAnswerEmpty(true);
-    console.log(questionData.answer.length);
+
+    if (!isAnswerEmpty) {
+      answerRegister();
+      window.location.reload(true);
+    }
   }
 
   const getDetail = () => {
     return axios
-      .get(`http://13.124.11.238:8080/question/${Number(id) + 1}/answer`, {
+      .get(`http://13.124.11.238:8080/question/${Number(id)}/answer`, {
         headers: {
           'Content-Type': `application/json`,
-          'ngrok-skip-browser-warning': true,
         },
       })
       .then((res) => {
@@ -43,6 +50,28 @@ export default function Answer({ questionData }) {
     getDetail();
   }, []);
 
+  const answerRegister = () => {
+    return axios
+      .post(
+        `http://13.124.11.238:8080/question/${id}?memberId=${memberId}`,
+        {
+          content: answer,
+        },
+        {
+          headers: {
+            'Content-Type': `application/json`,
+            Authorization: getCookie,
+          },
+        },
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch(() => {
+        alert('Something went wrong. Please try again.');
+      });
+  };
+
   return (
     <div className="answer__bg">
       {answerList.vote?.length > 1 ? (
@@ -54,8 +83,11 @@ export default function Answer({ questionData }) {
         ? answerList.vote.map((el) => (
             <div key={el.answerId} className="list answer__content-container">
               <div className="answer__content-wrap">
-                <Vote voteNumber={el.vote} />
-                <div className="answer__content">{el.content}</div>
+                <div className="answer__vote-content-wrap">
+                  <Vote voteNumber={el.vote} />
+                  <div className="answer__content">{el.content}</div>
+                </div>
+                <button className="detail__btn">Delete</button>
               </div>
               <div className="answer__bottom">
                 <div>
